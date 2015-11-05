b1d151f7c634ae19e821c1d0fb9e36ab5781f564
Shawn Neal <sneal@sneal.net>
2015-11-05 13:46:37 -0800

    Add ReSharper 10 installation support

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,13 @@
 
 default['resharper']['version'] = '7.1.3000.2254'
-
 default['resharper']['root_download_url'] = 'http://download.jetbrains.com/resharper'
+default['resharper']['vs_versions'] = %w( 10 11 12 14 )
+default['resharper']['products'] = 'dotPeek;teamCityAddin;ReSharper'
 
 default['resharper']['7.1.3000.2254']['checksum'] =
   '45e257e196f9d9953c58d866c74942df4958a5d83f32b3e56348f253aca21768'
 default['resharper']['7.1.3000.2254']['package_name'] = 'JetBrains ReSharper 7.1.3'
+
+default['resharper']['10']['checksum'] =
+  'ff33ad115d98f1a464c5340605f3bcd432168b99b6757720aca5c41856374c49'
+default['resharper']['10']['package_name'] = 'JetBrains ReSharper 10'
--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -1,10 +1,8 @@
 require 'minitest/spec'
 
 class TestResharperInstall < MiniTest::Chef::TestCase
-
   def test_resharper_was_installed
-    base_resharper_dir = File.join(ENV['ProgramFiles(x86)'], 'JetBrains', 'ReSharper')
+    base_resharper_dir = File.join(ENV['ProgramFiles(x86)'], 'JetBrains')
     assert Dir.exists?(base_resharper_dir)
   end
-
 end
