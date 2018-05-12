9634ebba89e34774494bfd95cf2be74b599b1153
Linus Ruth <linus.ruth@changepoint.com>
2018-05-11 17:37:24 -0700

    chore: update resharper version in test suite

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,12 +1,12 @@
 describe 'resharper::default' do
-  describe 'version 2016.2.2' do
+  describe 'version 2018.1' do
     let(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'windows', version: '2008R2').converge(described_recipe)
+      ChefSpec::ServerRunner.new(platform: 'windows', version: '2012R2').converge(described_recipe)
     end
     it 'download finished notifies install executable' do
-      resource = chef_run.remote_file('download_resharper_2016.2.2')
+      resource = chef_run.remote_file('download_resharper_2018.1')
       expect(resource).to notify(
-        'execute[install_resharper_2016.2.2]'
+        'execute[install_resharper_2018.1]'
       ).to(:run).immediately
     end
   end
